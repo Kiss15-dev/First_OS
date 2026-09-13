48d285b5b4100970bfa86fdcb4bb4bf0ee4e95a6
Kiss <aslan.veliyev.15@mail.ru>
2026-09-13 18:01:26 +0500

    Исправление pmm

--- pmm.c
+++ pmm.c
@@ -4,13 +4,14 @@
 static void* next_page = (void*)HEAP_START;
 
 void* pmm_alloc_page() {
+	if (next_page == NULL) return NULL
 	void* page = next_page;
 	
 	if ((uintptr_t)page + PAGE_SIZE <= (uintptr_t)HEAP_END) {
 		next_page = (void*)((uintptr_t)next_page + PAGE_SIZE);
-
-		return page;
+	} else {
+		next_page = NULL;
 	}
 
-	return NULL;
+	return page;
 }
