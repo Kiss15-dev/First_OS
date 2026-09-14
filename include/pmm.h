d11bc783270377e4d065674b9ad559dd65bb0c4e
Kiss <aslan.veliyev.15@mail.ru>
2026-09-14 15:41:06 +0500

    Написал для pmm освобождение страниц

--- include/pmm.h
+++ include/pmm.h
@@ -6,6 +6,8 @@
 #define HEAP_SIZE (4096 * 200)
 #define HEAP_END (HEAP_START + HEAP_SIZE)
 
+void pmm_init(void);
 void* pmm_alloc_page(void);
+void pmm_free_page(void* page_address);
 
 #endif
--- kernel/pmm.c
+++ kernel/pmm.c
@@ -1,10 +1,23 @@
 #include "types.h"
 #include "pmm.h"
 
-static void* next_page = (void*)HEAP_START;
+struct Page
+{
+	struct Page* next;
+};
+
+//static void* next_page = (void*)HEAP_START;
+
+static struct Page* free_pages_head = NULL;
+
+void pmm_init() {
+	for (uintptr_t page_address = HEAP_START; page_address + PAGE_SIZE <= HEAP_END; page_address += PAGE_SIZE) {
+		pmm_free_page((void*)page_address);
+	}
+}
 
 void* pmm_alloc_page() {
-	if (next_page == NULL) return NULL;
+	/*if (next_page == NULL) return NULL;
 	void* page = next_page;
 	
 	if ((uintptr_t)page + PAGE_SIZE <= (uintptr_t)HEAP_END) {
@@ -13,5 +26,22 @@ void* pmm_alloc_page() {
 		next_page = NULL;
 	}
 
-	return page;
+	return page;*/
+	if (free_pages_head == NULL) return NULL;
+
+	struct Page* allocated_memory = free_pages_head;
+
+	free_pages_head = free_pages_head->next;
+
+	return (void*)allocated_memory;
+}
+
+void pmm_free_page(void* page_address) {
+	if (page_address == NULL) return;
+
+	struct Page* released_page = (struct Page*)page_address;
+
+	released_page->next = free_pages_head;
+
+	free_pages_head = released_page;
 }
