98991da49494d92b68c7d7fe17d2fcdfb7e68361
Kiss <aslan.veliyev.15@mail.ru>
2026-09-18 14:21:50 +0500

    расширил таблицу записи ядра, заполнил полностью PD, перенёс адрес этой таблицы, адрес начала кучи

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,6 +1,6 @@
 [org 0x7c00]
 KERNEL_OFFSET equ 0x9000
-KERNEL_SECTORS equ 150
+KERNEL_SECTORS equ 100
 
 [bits 16]
 start:
@@ -30,7 +30,7 @@ start:
     or eax, 0x1
     mov cr0, eax
 
-    jmp CODE_SEG:init_pm
+    jmp PM_CODE_SEG:init_pm
 
 disk_error:
     jmp $
@@ -41,27 +41,39 @@ init_pm:
     mov ds, ax
     mov ss, ax
     mov es, ax
-    mov esp, 0x80000
+    mov esp, 0x500000
     mov ebp, esp
 
-    mov edi, 0x1000       
+    mov edi, 0x20000      
     mov cr3, edi          
     xor eax, eax          
-    mov ecx, 4096         
+    mov ecx, 527360
     rep stosd             
 
-    mov dword [0x1000], 0x2000 | 0x03
-    mov dword [0x1000 + 511 * 8], 0x2000 | 0x03
-    mov dword [0x1000 + 511 * 8 + 4], 0
-    mov dword [0x2000], 0x3000 | 0x03 
-    mov dword [0x3000], 0x4000 | 0x03 
+    mov dword [0x20000], 0x21000 | 0x03
+    mov dword [0x20000 + 4], 0
+    mov dword [0x20000 + 511 * 8], 0x21000 | 0x03
+    mov dword [0x20000 + 511 * 8 + 4], 0
 
-    mov edi, 0x4000       
-    mov ebx, 0x00000003   
-    mov ecx, 512          
-.loop_fill_pt:
+    mov dword [0x21000], 0x22000 | 0x03 
+    mov dword [0x21000 + 4], 0
+
+    mov edi, 0x22000
+    mov ebx, 0x23000 | 0x03
+    mov ecx, 512
+.loop_fill_pd:
     mov dword [edi], ebx
     mov dword [edi+4], 0  
+    add ebx, 4096         
+    add edi, 8            
+    loop .loop_fill_pd
+
+    mov edi, 0x23000       
+    mov ebx, 0x00000003   
+    mov ecx, 262144    
+.loop_fill_pt:
+    mov dword [edi], ebx
+    mov dword [edi+4], 0
     add ebx, 4096         
     add edi, 8            
     loop .loop_fill_pt
@@ -79,7 +91,7 @@ init_pm:
     or eax, 1 << 31
     mov cr0, eax
 
-    jmp CODE_SEG:init_lm
+    jmp LM_CODE_SEG:init_lm
 
 [bits 64]
 init_lm:
@@ -90,7 +102,7 @@ init_lm:
     mov fs, ax
     mov gs, ax
 
-    mov rsp, 0xFFFFFFFF80080000
+    mov rsp, 0xFFFFFFFF80500000
     mov rbp, rsp
 
     mov rbx, 0xFFFFFFFF80000000 + KERNEL_OFFSET
@@ -100,13 +112,13 @@ align 8
 gdt_start:
     dq 0x0                  
 
-gdt_code:               
+gdt_pm_code:               
     dw 0xFFFF               
     dw 0x0000               
     db 0x00                 
-    db 10011010b            
-    db 00101111b            
-    db 0x00                 
+    db 10011010b            ; Present, Ring 0, Code, Executable, Readable
+    db 11001111b            ; G=1 (4KB granularity), D=1 (32-bit mode) <--- ТУТ ИЗМЕНЕНО
+    db 0x00                
 
 gdt_data:               
     dw 0xFFFF
@@ -115,14 +127,23 @@ gdt_data:
     db 10010010b            
     db 11001111b            
     db 0x00
+
+gdt_lm_code:               
+    dw 0xFFFF               
+    dw 0x0000               
+    db 0x00                 
+    db 10011010b            ; Present, Ring 0, Code, Executable, Readable
+    db 00101111b            ; L=1 (64-bit Long Mode code segment), D=0
+    db 0x00   
 gdt_end:
 
 gdt_descriptor:
     dw gdt_end - gdt_start - 1 
     dd gdt_start            
 
-CODE_SEG equ gdt_code - gdt_start 
-DATA_SEG equ gdt_data - gdt_start 
+PM_CODE_SEG equ gdt_pm_code - gdt_start 
+DATA_SEG equ gdt_data - gdt_start
+LM_CODE_SEG equ gdt_lm_code - gdt_start 
 
 times 510 - ($ - $$) db 0
 dw 0xaa55
